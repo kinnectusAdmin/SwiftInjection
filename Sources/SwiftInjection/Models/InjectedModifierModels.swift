5148301dbdbaddb8a4c3b7dc973d89b392e8276f
Blake <blake.rogers757@gmail.com>
2023-09-25 14:29:21 -0400

    relocate InjectedModifier specialized models

---
+++ Sources/SwiftInjection/Models/InjectedModifierModels.swift
@@ -0,0 +1,97 @@
+//
+//  InjectedModifierModels.swift
+//  
+//
+//  Created by Blake Osonduagwueki on 9/25/23.
+//
+
+import Foundation
+import SwiftUI
+public enum InjectedFontDesign: String, Codable  {
+    case none
+    var render: Font.Design {
+        return .default
+    }
+}
+public enum InjectedFontWidth: String, Codable {
+    case none
+    var render: Font.Width {
+        return .compressed
+    }
+}
+public enum InjectedTextLineStylePattern: String, Codable {
+    case none
+    var render: Text.LineStyle.Pattern {
+        return .dash
+    }
+}
+public enum InjectedTextTruncation: String, Codable {
+    case none
+    var render: Text.TruncationMode {
+        return .head
+    }
+}
+public enum InjectedBlendMode: String, Codable {
+    case  none
+    var render: BlendMode {
+        .color
+    }
+}
+public enum InjectedTextAlignment: String, Codable {
+    case none
+    var render: TextAlignment {
+        .center
+    }
+}
+public enum InjectedImageScale: String, Codable {
+    case none
+    var render: Image.Scale {
+        .medium
+    }
+}
+public enum InjectedAnimation: String, Codable {
+    case none
+    var render: Animation {
+        return .default
+    }
+}
+public enum InjectedButtonStyle: String, Codable {
+    case none
+    var render: any PrimitiveButtonStyle {
+        DefaultButtonStyle()
+    }
+}
+public enum InjectedProgressViewStyle: String, Codable {
+    case none
+}
+public enum InjectedPickerStyle: String, Codable {
+    case none
+    var render: any PickerStyle {
+        DefaultPickerStyle()
+    }
+}
+public enum InjectedSymbolRenderingMode: String, Codable {
+    case none
+    var render: SymbolRenderingMode {
+        .hierarchical
+    }
+}
+public enum InjectedResizingMode: String, Codable {
+    case none
+    var render: Image.ResizingMode {
+        .stretch
+    }
+}
+public enum InjectedImageRenderingMode: String, Codable {
+    case none
+    var render: Image.TemplateRenderingMode {
+        .original
+    }
+}
+public enum InjectdDatePickerStyle: String, Codable {
+    case none
+    var render: any DatePickerStyle {
+        DefaultDatePickerStyle()
+    }
+}
+ 
