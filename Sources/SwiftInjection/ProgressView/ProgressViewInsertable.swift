4c612c3d3fddc7d93dcc7b386e18ab491fe03a53
Blake <blake.rogers757@gmail.com>
2023-10-09 16:50:44 -0400

    add modifiers for progress view

--- Sources/SwiftInjection/ProgressView/ProgressViewInsertable.swift
+++ Sources/SwiftInjection/ProgressView/ProgressViewInsertable.swift
@@ -15,30 +15,30 @@ struct ProgressViewInsertable: View {
     var body: some View {
         switch store.composition {
         case .standard:
-            ProgressView()
+            ProgressView().addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
         case .title(let title):
             
-            ProgressView(title)
+            ProgressView(title).addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case .title_value(let title, let value, let total):
             
-            ProgressView(title, value: Float(value), total: Float(total))
+            ProgressView(title, value: Float(value), total: Float(total)).addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case .label(let label):
             
             ProgressView {
                 Insertable(state: store.stateSubject, container: container, viewStore: label)
-            }
+            }.addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case .timerInterval(let lowerDate, let upperDate):
             
-            ProgressView(timerInterval: ClosedRange(uncheckedBounds: (lowerDate,upperDate)))
+            ProgressView(timerInterval: ClosedRange(uncheckedBounds: (lowerDate,upperDate))).addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case .timerInterval_label(let lowerDate, let upperDate, let label):
             
             ProgressView(timerInterval: ClosedRange(uncheckedBounds: (lowerDate, upperDate))) {
                 Insertable(state: store.stateSubject, container: container, viewStore: label)
-            }
+            }.addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case let .timerInterval_countsdown_label_currentValueLabel(lowerDate,
                                                               upperDate,
@@ -50,17 +50,17 @@ struct ProgressViewInsertable: View {
                 Insertable(state: store.stateSubject, container: container, viewStore: label)
             } currentValueLabel: {
                 Insertable(state: store.stateSubject, container: container, viewStore: currentValueLabel)
-            }
+            }.addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case let .value_total(value, total):
             
-            ProgressView.init(value: Float(value), total: Float(total))
+            ProgressView.init(value: Float(value), total: Float(total)).addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case let .value_total_label(value, total, label):
             
             ProgressView.init(value: Float(value), total: Float(total)) {
                 Insertable(state: store.stateSubject, container: container, viewStore: label)
-            }
+            }.addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
             
         case let .value_total_label_currentValueLabel(value, total, label, currentValueLabel):
             
@@ -68,7 +68,7 @@ struct ProgressViewInsertable: View {
                 Insertable(state: store.stateSubject, container: container, viewStore: label)
             } currentValueLabel: {
                 Insertable(state: store.stateSubject, container: container, viewStore: currentValueLabel)
-            }
+            }.addModifiers(mods: store.viewStore.modifiers, state: store.stateSubject, container: container)
 
             
         }
